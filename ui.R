48aaa6e6410b2380cc1dcc9ff6f6a1593fa56eab
Olivia Fu <olivia@Olivias-MacBook-Pro.local>
2020-09-25 20:05:43 +0900

    olivia styling

--- ui.R
+++ ui.R
@@ -118,7 +118,7 @@ ui <- dashboardPage(title="CovidU",
   
   dashboardHeader(
     title = dashboardLogo,
-    titleWidth = 450
+    titleWidth = 350
   ),
   
   dashboardSidebar(
@@ -437,27 +437,22 @@ ui <- dashboardPage(title="CovidU",
   
   dashboardBody(
     dashboardTheme,
-    fluidRow(
-    tags$div(tags$body(tags$strong("How To Use:"), "The menu to the left allows you to set some parameters of the model - hit \"Recompute\" when finished (it's pre-populated with what we think is a moderately conservative scenario with frequent testing)."))
-    ),
+    tags$header(tags$strong("How To Use:"), "The menu to the left allows you to set some parameters of the model - hit \"Recompute\" when finished (it's pre-populated with what we think is a moderately conservative scenario with frequent testing)."),
     fluidRow(
       box(width=15, plotlyOutput('plot'))),
     fluidRow(
-      box(width=15, DT::dataTableOutput("tabledata")))#,
-    # fluidRow(
-    #   tags$div(tags$body(tags$strong("Caveat:"),"This model is an illustrative tool, and is not meant to generate accurate predictions. Many simplifying assumptions have been made in order to highlight a few important dynamics.")),
-    #   tags$div(tags$body(
-    #     "Dashboard by Olivia Fu,",
-    #     tags$a(href="gking.harvard.edu", "Gary King,"),
-    #     tags$a(href="https://projects.iq.harvard.edu/zmukerjee", "Zagreb Mukerjee."),
-    #     "Based on",
-    #     tags$a(href="https://jamanetwork.com/journals/jamanetworkopen/fullarticle/2768923", "this paper."),
-    #     "For more information, see",
-    #     tags$a(href="brokenlink", "here.")
-    #   ))
-    # )
-   
-    
+      box(width=15, DT::dataTableOutput("tabledata"))),
+    tags$footer(tags$strong("Caveat:"),"This model is an illustrative tool, and is not meant to generate accurate predictions. Many simplifying assumptions have been made in order to highlight a few important dynamics."),
+    br(),
+    tags$footer(
+      "Dashboard by Olivia Fu,",
+      tags$a(href="https://gking.harvard.edu", "Gary King,"),
+      tags$a(href="https://projects.iq.harvard.edu/zmukerjee", "Zagreb Mukerjee."),
+      "Based on",
+      tags$a(href="https://jamanetwork.com/journals/jamanetworkopen/fullarticle/2768923", "this paper."),
+      "For more information, see",
+      tags$a(href="brokenlink", "here.")
+    )
   )
   
   
