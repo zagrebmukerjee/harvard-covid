792d4655025944175e4006ff5e1e7386ba447a2a
Zagreb Mukerjee <zagrebmukerjee@gmail.com>
2021-01-16 01:29:12 -0500

    typo fix

--- www/script.js
+++ www/script.js
@@ -8,7 +8,7 @@ function getCookies(){
 Shiny.addCustomMessageHandler('cookie-set', function(msg){
   
   tmp = msg.name + "=" + msg.value
-  tmp += "; max-age=" + (30*24*60*60) + "; " + "path=/"+ "domain=projects.iq.harvard.edu;";
+  tmp += "; max-age=" + (30*24*60*60) + "; " + "path=/;'"+ "domain=projects.iq.harvard.edu;";
   document.cookie = tmp
   
   
